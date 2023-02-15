1faec5ec729ec54979f380bb41c8fc1e78884473
Наталья Буренина <bur_nat_nev@mail.ru>
2023-02-15 11:26:04 +0300

    Реализовано первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,27 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = b**2 - 4*a*c;
+   if(a==0) return
+   
+     let x1;
+     let x2;
+          if(d === 0){
+          x1 =-b/(2*a);
+         arr.push(x1);
+          console.log(arr);
+  return arr;
+      }
+        if(d > 0){
+          x1 = (-b + Math.sqrt(d)) / 2*a;
+          x2 = (-b - Math.sqrt(d)) / 2*a;
+          arr.push(x1,x2);
+    console.log(arr);
+    return arr;
+        }
+        if(d < 0){
+          console.log(arr)
+        }
   return arr;
 }
 
