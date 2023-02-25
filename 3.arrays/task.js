d0b17663ea9263c45061971ee14f4797e04f3b9b
Наталья Буренина <bur_nat_nev@mail.ru>
2023-02-25 12:07:48 +0300

    Домашнее задание к лекции 3 Массивы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
+// Задача 1
+
 function compareArrays(arr1, arr2) {
-  
+	return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
 }
 
+//Задача 2
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+	return users.
+	filter(user => user.gender === gender).
+	map(user => user.age).
+	reduce((averageValue, userAge, index, arr) => averageValue + userAge / arr.length, 0);
 }
